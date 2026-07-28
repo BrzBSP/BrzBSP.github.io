7682d589679b3389405a86b5f406a6365f53eee3
BrzBSP <brz_bsp_pgcpf@carrefour.com>
2026-07-28 12:57:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -19,87 +19,26 @@ async function carregarDestaques() {
     return destaques;
 }
 
+async function carregarMenu() {
+    const resposta = await fetch("./dados/Menu.csv");
+    const texto = await resposta.text();
 
+    const linhas = texto.trim().split("\n");
+    const cabecalho = linhas.shift().split(",");
 
+    const destaques = linhas.map(linha => {
+        const valores = linha.split(",");
 
+        return {
+            nome: valores[0],
+            descricao: valores[1].replace(/^"|"$/g, ""),
+            preco: parseFloat(valores[2]),
+            imagem: valores[3]
+        };
+    });
 
-
-const produtos = [
-
-{
-    nome:"Espresso Tradicionala",
-
-    descricao:"Café encorpado preparado com grãos especiais selecionados.",
-
-    preco:6.90,
-
-    imagem:"./img/cheescake.png"
-},
-
-{
-    nome:"Espresso Tradicionala",
-
-    descricao:"Café encorpado preparado com grãos especiais selecionados.",
-
-    preco:6.90,
-
-    imagem:"./img/cheescake.png"
-},
-
-{
-    nome:"Espresso Tradicionala",
-
-    descricao:"Café encorpado preparado com grãos especiais selecionados.",
-
-    preco:6.90,
-
-    imagem:"./img/cheescake.png"
-},
-
-{
-    nome:"Espresso Tradicionala",
-
-    descricao:"Café encorpado preparado com grãos especiais selecionados.",
-
-    preco:6.90,
-
-    imagem:"./img/cheescake.png"
-},
-
-{
-    nome:"Espresso Tradicionala",
-
-    descricao:"Café encorpado preparado com grãos especiais selecionados.",
-
-    preco:6.90,
-
-    imagem:"./img/cheescake.png"
+    return menu;
 }
-
-];
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -129,41 +68,34 @@ async function carregarProdutos() {
             </div>
         `;
     });
+
+
+    const menu = await carregarMenu();
+
+    const me = document.getElementById("menu");
+
+    me.innerHTML = "";
+
+    menu.forEach(produto => {
+        dest.innerHTML += `
+            <div class="card">
+                <img src="${produto.imagem}" alt="${produto.nome}">
+
+                <div class="info">
+                    <h2>${produto.nome}</h2>
+
+                    <p>${produto.descricao}</p>
+
+                    <div class="preco">
+                        <span class="valor">
+                            R$ ${produto.preco.toFixed(2).replace(".", ",")}
+                        </span>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+
 }
 
 carregarProdutos();
-
-
-
-const menu = document.getElementById("menu");
-
-produtos.forEach(produto=>{
-
-menu.innerHTML+=`
-
-<div class="card">
-
-<img src="${produto.imagem}" alt="${produto.nome}">
-
-<div class="info">
-
-<h2>${produto.nome}</h2>
-
-<p>${produto.descricao}</p>
-
-<div class="preco">
-
-<span class="valor">
-R$ ${produto.preco.toFixed(2).replace(".",",")}
-</span>
-
-</div>
-
-</div>
-
-</div>
-
-`;
-
-});
-
